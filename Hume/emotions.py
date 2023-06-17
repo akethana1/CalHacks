334834e01758780faeb7fbd2ddc96162dca8e5cc
Vrushank Prakash <prakash.vrushank.1117@gmail.com>
2023-06-17 16:45:11 -0700

    got language to work

--- Hume/emotions.py
+++ Hume/emotions.py
@@ -1,30 +1,28 @@
-#from utilities import print_emotions
-
 from hume import HumeBatchClient
-from hume.models.config import ProsodyConfig
-from pprint import pprint
+from hume.models.config import LanguageConfig
+from itertools import islice
 
 client = HumeBatchClient("ZH01lHke6S8yBjZAAr05j3elbMkM6UwPJlGH2cH0HzBaOeoa")
 urls = ["https://storage.googleapis.com/hume-test-data/audio/ninth-century-laugh.mp3"]
-config = ProsodyConfig()
+config = LanguageConfig()
 job = client.submit_job(urls, [config])
 
 print(job)
 print("Running...")
 
 job.await_complete()
-predictions = job.get_predictions()
-print(predictions)
 
 full_predictions = job.get_predictions()
 for source in full_predictions:
     source_name = source["source"]["url"]
     predictions = source["results"]["predictions"]
     for prediction in predictions:
-        print()
-        print("Speech prosody")
-        prosody_predictions = prediction["models"]["prosody"]["grouped_predictions"]
-        for prosody_prediction in prosody_predictions:
-            for segment in prosody_prediction["predictions"][:1]:
-                print(segment["emotions"])
-                print ("\n")
+        language_predictions = prediction["models"]["language"]["grouped_predictions"]
+        for language_prediction in language_predictions:
+            for segment in language_prediction["predictions"][:1]:
+                entries = segment["emotions"]
+                emotions = {entry['name'] : entry['score'] for entry in entries}
+                top_emotions = dict(sorted(emotions.items(), key=lambda x: x[1], reverse=True)[:5])
+                print(top_emotions)
+
+                
