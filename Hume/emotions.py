5bd1d9016252081f066c0b924e0c476725fd93fd
arnavkethana <kethana.arnav@gmail.com>
2023-06-17 12:48:09 -0700

    brah

--- Hume/emotions.py
+++ Hume/emotions.py
@@ -1,7 +1,30 @@
 from hume import HumeBatchClient
 from hume.models.config import FaceConfig
+from pprint import pprint
 
-client = HumeBatchClient("<YOUR-API-KEY>")
+client = HumeBatchClient("ZH01lHke6S8yBjZAAr05j3elbMkM6UwPJlGH2cH0HzBaOeoa")
 urls = ["https://iep.utm.edu/wp-content/media/hume-bust.jpg"]
 config = FaceConfig()
 job = client.submit_job(urls, [config])
+
+print(job)
+print("Running...")
+
+job.await_complete()
+predictions = job.get_predictions()
+print(predictions)
+
+details = job.get_details()
+run_time_ms = details.get_run_time_ms()
+print(f"Job ran for {run_time_ms} milliseconds")
+
+'''status = job.get_status()
+print(f"Job status: {status}")
+
+details = job.get_details()
+run_time_ms = details.get_run_time_ms()
+print(f"Job ran for {run_time_ms} milliseconds")
+
+#predictions = job.get_predictions()
+#pprint(predictions)
+'''
